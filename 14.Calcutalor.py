803c2cec36e77ff463d57d60d9a7e0ad6aea7b15
memuth <60926408+memuth@users.noreply.github.com>
2021-10-04 18:38:37 +0530

    Update 14.Calcutalor.py

--- 14.Calcutalor.py
+++ 14.Calcutalor.py
@@ -1,12 +1,44 @@
-n = int(input())
+# Program for basic caluclations
+print("*************Basic Calculator*************")
+print("1 for addition")
+print("2 for subtraction")
+print("3 for multiplication")
+print("4 for division")
+print("5 for modulo")
+
 while True:
-  a = raw_input()
-  a = a.split(' ')
-  if(a[0] == '+'):
-    n = n + int(a[1])
-  if(a[0] == '*'):
-    n = n * int(a[1])
-  if(a[0] == '%'):
-    n = n % int(a[1])
-    break
-print n
+    x = int(input("Enter first number:"))
+    y = int(input("Enter second number:"))
+    while True:
+        choice = int(input("Enter your choice:"))
+        if choice == 1:
+            print(x+y)
+        elif choice == 2:
+            print(x-y)
+        elif choice == 3:
+            print(x*y)
+        elif choice == 4:
+            print(x/y)
+        elif choice == 5:
+            print(x%y)
+        else:
+            print("Enter a valid choice!")
+            continue
+        while True:
+            ch = input("Do you want to execute another operation with SAME NUMBER then Press 'Y' for yes or Press 'N' for No:")
+            if ch == 'y' or ch == 'Y':
+                break
+            elif ch == 'n' or ch == 'N':
+                break
+            else:
+                print("Invalid choice!")
+        if ch == 'y' or ch == 'Y':
+            continue
+        else:
+            break
+    exit = input("Do you want to execute another operation then Press 'Y' for yes or Press 'N' for No to exit:")
+    if exit == 'y' or ch == 'Y':
+        continue
+    else:
+        break
+print("______Thank You for using our application._______")
